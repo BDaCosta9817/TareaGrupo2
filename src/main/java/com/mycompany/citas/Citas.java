00890f55ac11456c23f79cb5b7aede28c7d58e2b
UserCP <UserCP@DESKTOP-EVBJTP8>
2024-11-03 21:26:06 -0600

    Carga inicial TareaGrupo

---
+++ src/main/java/com/mycompany/citas/Citas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.citas;
+
+/**
+ *
+ * @author UserCP
+ */
+public class Citas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
