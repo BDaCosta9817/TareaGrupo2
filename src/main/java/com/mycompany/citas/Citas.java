6a55df74dbd9bff07e32e75b7a54a47135b84506
BDaCosta9817 <dacostagomezblanca9998@gmail.com>
2024-11-04 21:48:29 -0600

    Update Citas.java

--- src/main/java/com/mycompany/citas/Citas.java
+++ src/main/java/com/mycompany/citas/Citas.java
@@ -1,5 +1,9 @@
 /*
  * Blanca Da Costa Gomez Carcamo
+ *04/11/2024
+ * Nuestro programa consiste en permitir que el   usuario agende una cita ingresando sus datos personales. El usuario podrá elegir entre dos tipos de cita con un doctor o 
+ *como deportista. Si elige la cita con el doctor, podrá seleccionar la especialidad médica m y elegir el día de la cita  en el caso de la cita para deportista podrá seleccionar
+ *el deporte que practica, la fecha de la cita  y especificar su nivel de habilidad en ese deporte. Al final, todas las citas programadas se mostrarán en pantalla.
  */
 
 
