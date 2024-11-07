8ecc37d8a69402e7c078b4e197b00370a5a26b3d
FerMendoza22 <lfayala@uth.hn>
2024-11-06 21:40:54 -0600

    Update Deportista.java

--- src/main/java/com/mycompany/citas/Deportista.java
+++ src/main/java/com/mycompany/citas/Deportista.java
@@ -8,6 +8,33 @@ package com.mycompany.citas;
  *
  * @author UserCP
  */
-public class Deportista {
-    
+package com.mycompany.deportista;
+
+/**
+ *
+ * @author DELL VOSTRO
+ */
+public class Deportista extends Persona {
+    private final String deporte;
+    private final int habilidad;
+
+    public Deportista(String nombre, int edad, String genero, String direccion, String cinta, String deporte, int habilidad) {
+        super(nombre, edad, genero, direccion, cinta);
+        this.deporte = deporte;
+        this.habilidad = habilidad;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public int getHabilidad() {
+        return habilidad;
+    }
+
+    public void mostrarDetalles() {
+        mostrarInformacion();
+        System.out.println("Deporte: " + deporte);
+        System.out.println("Nivel de habilidad: " + habilidad);
+    }
 }
