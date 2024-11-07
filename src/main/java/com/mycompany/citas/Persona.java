f4f322c97df88f837e6fca9d1c91ccb738179f07
BDaCosta9817 <dacostagomezblanca9998@gmail.com>
2024-11-06 19:19:28 -0600

    Update Persona.java

--- src/main/java/com/mycompany/citas/Persona.java
+++ src/main/java/com/mycompany/citas/Persona.java
@@ -4,33 +4,3 @@
  */
 
 
-package com.mycompany.citas;
-
-public class Persona {
-    private final String nombre;
-    private final int edad;
-    private final String genero;
-    private final String direccion;
-    private final String cinta;
-
-    public Persona(String nombre, int edad, String genero, String direccion, String cinta) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.genero = genero;
-        this.direccion = direccion;
-        this.cinta = cinta;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void mostrarInformacion() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Genero: " + genero);
-        System.out.println("Direccion: " + direccion);
-        System.out.println("Cinta: " + cinta);
-    }
-}
-
